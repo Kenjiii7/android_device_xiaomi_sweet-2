7622007b90d742c3523da3063a788040d6c66132
mspjason <mspcrew@gmail.com>
2021-07-26 11:34:45 +0200

    sweet: product.prop: add dex2oat & time props

--- product_prop.mk
+++ product_prop.mk
@@ -45,3 +45,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.ims.disableDebugLogs=1 \
 	persist.vendor.ims.disableIMSLogs=1 \
 	persist.vendor.ims.disableQXDMLogs=1
+	
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+dalvik.vm.boot-dex2oat-threads=8 \
+dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+dalvik.vm.dex2oat-threads=8 \
+dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+dalvik.vm.image-dex2oat-threads=8
+
+# Time
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.delta_time.enable=true 
